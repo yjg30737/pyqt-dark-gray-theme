1cd233080216f872e9ca9f853fbd036a14f0efcb
Jung Gyu Yoon <yjg30737@gmail.com>
2022-02-25 15:16:24 +0900

    Show return type explicitly

--- pyqt_dark_gray_theme/darkGrayTheme.py
+++ pyqt_dark_gray_theme/darkGrayTheme.py
@@ -1,7 +1,7 @@
 import os, inspect, sys
 
 
-def getDarkGrayTheme():
+def getDarkGrayTheme() -> str:
     caller_path = os.path.dirname(inspect.getframeinfo(sys._getframe(0)).filename)
     f = open(os.path.join(caller_path, 'dark_gray_theme.css'), 'r')
     content = f.read()
