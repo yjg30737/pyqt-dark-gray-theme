3e3ae269628dd7e1a80de582c5bddfc453a1ca8c
Jung Gyu Yoon <yjg30737@gmail.com>
2022-02-25 21:00:44 +0900

    Rename all methods

--- pyqt_dark_gray_theme/darkGrayTheme.py
+++ pyqt_dark_gray_theme/darkGrayTheme.py
@@ -1,19 +1,19 @@
 import os, inspect, sys
 
 
-def getDarkGrayTheme() -> str:
+def getThemeStyle() -> str:
     caller_path = os.path.dirname(inspect.getframeinfo(sys._getframe(0)).filename)
     f = open(os.path.join(caller_path, 'dark_gray_theme.css'), 'r')
     content = f.read()
     return content
 
-def getDarkGrayIconButtonStyle() -> str:
+def getIconButtonStyle() -> str:
     caller_path = os.path.dirname(inspect.getframeinfo(sys._getframe(0)).filename)
     f = open(os.path.join(caller_path, 'icon_button.css'), 'r')
     content = f.read()
     return content
 
-def getDarkGrayNoIconButtonStyle() -> str:
+def getNoIconButtonStyle() -> str:
     caller_path = os.path.dirname(inspect.getframeinfo(sys._getframe(0)).filename)
     f = open(os.path.join(caller_path, 'no_icon_button.css'), 'r')
     content = f.read()
