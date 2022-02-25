e22292130747e7fbee6b9c6078e970d1c94e48a6
Jung Gyu Yoon <yjg30737@gmail.com>
2022-02-25 15:15:54 +0900

    Get relative path of css file based on main module to prevent FileNotFoundError

--- pyqt_dark_gray_theme/darkGrayTheme.py
+++ pyqt_dark_gray_theme/darkGrayTheme.py
@@ -1,4 +1,8 @@
+import os, inspect, sys
+
+
 def getDarkGrayTheme():
-    f = open('dark_gray_theme.css', 'r')
+    caller_path = os.path.dirname(inspect.getframeinfo(sys._getframe(0)).filename)
+    f = open(os.path.join(caller_path, 'dark_gray_theme.css'), 'r')
     content = f.read()
     return content
