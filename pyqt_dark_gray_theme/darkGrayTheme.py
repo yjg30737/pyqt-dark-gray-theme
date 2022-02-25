a85e5d54039828d6973f47b6dbdcbed66e6bf876
Jung Gyu Yoon <yjg30737@gmail.com>
2022-02-25 21:00:05 +0900

    Add new methods (getDarkGrayIconButtonStyle, getDarkGrayNoIconButtonStyle)

--- pyqt_dark_gray_theme/darkGrayTheme.py
+++ pyqt_dark_gray_theme/darkGrayTheme.py
@@ -6,3 +6,15 @@ def getDarkGrayTheme() -> str:
     f = open(os.path.join(caller_path, 'dark_gray_theme.css'), 'r')
     content = f.read()
     return content
+
+def getDarkGrayIconButtonStyle() -> str:
+    caller_path = os.path.dirname(inspect.getframeinfo(sys._getframe(0)).filename)
+    f = open(os.path.join(caller_path, 'icon_button.css'), 'r')
+    content = f.read()
+    return content
+
+def getDarkGrayNoIconButtonStyle() -> str:
+    caller_path = os.path.dirname(inspect.getframeinfo(sys._getframe(0)).filename)
+    f = open(os.path.join(caller_path, 'no_icon_button.css'), 'r')
+    content = f.read()
+    return content
