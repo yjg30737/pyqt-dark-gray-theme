8400baf4a16773599466205e39ea4db4a0181df7
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-12 10:45:51 +0900

    Update requirements.txt and setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-dark-gray-theme',
@@ -9,8 +16,10 @@ setup(
     packages=find_packages(),
     description='PyQt dark-gray theme',
     url='https://github.com/yjg30737/pyqt-dark-gray-theme.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.8',
-        'qt-sass-theme-getter @ git+https://git@github.com/yjg30737/qt-sass-theme-getter.git@main'
+        'qt-sass-theme-getter>=0.0.1'
     ]
 )
